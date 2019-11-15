0a1b5d0430858ab39f18f6709a7ef80ab4b83c82
Fernando9825 <fer25alvarado@gmail.com>
2019-11-15 10:41:37 -0600

    Municipios is back to contribuyenteController

--- src/main/java/com/fernando9825/alcaldiasvrestapi/models/entity/Contribuyente.java
+++ src/main/java/com/fernando9825/alcaldiasvrestapi/models/entity/Contribuyente.java
@@ -67,7 +67,7 @@ public class Contribuyente implements Serializable {
     @JoinColumn(name = "institucion_id_fk", referencedColumnName = "id", nullable = false)
     private Institucion institucion;
 
-    @JsonIgnore
+    
     @OneToOne
     @JoinColumn(name = "municipio_id_fk", referencedColumnName = "id", nullable = false)
     private Municipio municipio;
